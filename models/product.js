2a9730fcd944f9ad0f32bc192c7735e1af06775d
PrimeWalkerVN <nchithanh609@gmail.com>
2019-11-19 09:29:41 +0700

    change field product

--- models/product.js
+++ models/product.js
@@ -2,9 +2,10 @@ let mongoose = require('mongoose');
 let Schema = mongoose.Schema
 let schema = new Schema({
     id: {type: Number, required: true},
-    imagePath: {type: String, required: true},
     title: {type: String, required: true},
     price: {type: Number, required: true},
+    typeProduct: {type: String, required: true},
+    imagePath: {type: String, required: true},
 });
 
  module.exports = mongoose.model('Product',schema, 'products');
