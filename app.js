e3ce70c1b08ea08ff770cb15d9f2dc5342636fac
maihuythong <maihuythong99@gmail.com>
2019-11-28 09:37:46 +0700

    Chuc nang login, register web khach hang

--- app.js
+++ app.js
@@ -3,30 +3,57 @@ let express = require('express');
 let path = require('path');
 let cookieParser = require('cookie-parser');
 let logger = require('morgan');
+
+let userRoutes = require('./routes/users');
 let indexRouter = require('./routes/index');
 let categoryRouter = require('./routes/category');
 let singleProductRouter = require('./routes/single-product');
 let cartRouter = require('./routes/cart');
 let checkoutRouter = require('./routes/checkout');
 let confirmationRouter = require('./routes/confirmation');
-let loginRouter = require('./routes/login');
 let trackingRouter = require('./routes/tracking');
-let registrationRouter = require('./routes/registration');
 let forgotRouter = require('./routes/forgot');
-let profileRouter = require('./routes/profile');
 let mongoose = require('mongoose');
 require('dotenv').config();
 
-mongoose.connect(process.env.DB_URL, { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect(process.env.DB_URL_LOCAL, { useNewUrlParser: true, useUnifiedTopology: true });
 let db = mongoose.connection;
 db.on('error',console.error.bind(console, 'MongoDB connection error.....'));
 
 
+const flash = require('connect-flash');
+const session = require('express-session');
+const passport = require('passport');
+
 let app = express();
+
+//passport config
+require('./passport')(passport);
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
+
+// Express Session
+app.use(session({
+  secret: 'secret',
+  resave: true,
+  saveUninitialized: true
+}));
+
+// passport middleware
+app.use(passport.initialize());
+app.use(passport.session());
+
+// Connect flash
+app.use(flash());
+
+// global vars
+app.use((req, res, next) => {
+  res.locals.success_msg = req.flash('success_msg');
+  res.locals.error_msg = req.flash('error_msg');
+  next();
+});
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -34,17 +61,15 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
+app.use('/users', require('./routes/users'));
 app.use('/index', indexRouter);
 app.use('/category', categoryRouter);
 app.use('/single-product',singleProductRouter);
 app.use('/checkout',checkoutRouter);
 app.use('/cart',cartRouter);
 app.use('/confirmation',confirmationRouter);
-app.use('/login',loginRouter);
 app.use('/tracking',trackingRouter);
-app.use('/registration',registrationRouter);
 app.use('/forgot',forgotRouter);
-app.use('/profile',profileRouter);
 
 
 // catch 404 and forward to error handler
---
+++ routes/users.js
@@ -0,0 +1,94 @@
+const express = require('express');
+const router = express.Router();
+const bcrypt = require('bcryptjs');
+const passport = require('passport');
+
+const User = require('../models/Users');
+
+router.get('/login', (req, res) => res.render('login'));
+
+router.get('/registration', (req, res) => res.render('registration'));
+
+//register post handle
+router.post('/registration', (req, res) => {
+    const { name, email, password, password2} = req.body;
+
+    let errors = [];
+    
+    if(!name || !email || !password || !password2){
+        errors.push({msg: 'Vui lòng nhập đầy đủ thông tin!'});
+    }
+
+    if(password !== password2){
+        errors.push({msg: 'Xác nhận mặt khẩu không đúng'});
+    }
+
+    if(password.lengh < 6){
+        errors.push({msg: 'Mật khẩu phải dài hơn 6 ký tự'});
+    }
+
+    if(errors.length > 0){
+        res.render('registration',{
+            errors,
+            name,
+            email,
+            password,
+            password2
+        });
+    }else{
+        
+        User.findOne({email: email})
+            .then(user => {
+                if(user){
+                    // user exist
+                    errors.push({msg: 'Email đã tồn tại'})
+                    res.render('registration',{
+                        errors,
+                        name,
+                        email,
+                        password,
+                        password2
+                    });
+                }else{
+                    const newUser = new User({
+                        name,
+                        email,
+                        password
+                    });
+
+                    //Hash password
+                    bcrypt.genSalt(10, (err, salt) =>
+                        bcrypt.hash(newUser.password, salt, (err, hash) => {
+                            if(err) throw err;
+                            //set new password
+                            newUser.password = hash;
+                            //save
+                            newUser.save()
+                                .then(user => {
+                                    req.flash('success_msg', 'Bạn đã đăng ký thành công, có thể đăng nhập ngay bây giờ')
+                                    res.redirect('/users/login');
+                                })
+                                .catch(err => console.log(err));
+                        }))
+                }
+            });
+    }
+
+});
+
+// login post handle
+router.post('/login', (req, res, next) => {
+    passport.authenticate('local', {
+        successRedirect: '/',
+        failureRedirect: 'login',
+        failureFlash: true
+    })(req, res, next);
+});
+
+//logout
+router.get('/logout', (req, res) => {
+    req.logout();
+    res.redirect('/users/login')
+})
+
+module.exports = router;
